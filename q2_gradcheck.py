8f6262696c84a5b9124aba849adec1648bc007c1
fucusy <fucus@qq.com>
2016-07-16 15:44:11 +0800

    move gradchekc function out

--- q2_gradcheck.py
+++ q2_gradcheck.py
@@ -1,63 +1,10 @@
 import numpy as np
-import random
+from q2_gradcheck_fuc import gradcheck_naive
+
+
 from q1_softmax import softmax_loss_grad, softmax
 from q2_sigmoid import sigmoid_loss_grad
 from q2_function import cross_category_loss_grad, score_to_loss_grad
-
-
-# First implement a gradient checker by filling in the following functions
-def gradcheck_naive(f, x):
-    """ 
-    Gradient check for a function f 
-    - f should be a function that takes a single argument and outputs the cost and its gradients
-    - x is the point (numpy array) to check the gradient at
-    """ 
-
-    rndstate = random.getstate()
-    random.setstate(rndstate)  
-    fx, grad = f(x) # Evaluate function value at original point
-    h = 1e-4
-
-    # Iterate over all indexes in x
-    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
-    while not it.finished:
-        ix = it.multi_index
-
-        ### try modifying x[ix] with h defined above to compute numerical gradients
-        ### make sure you call random.setstate(rndstate) before calling f(x) each time, this will make it 
-        ### possible to test cost functions with built in randomness later
-
-
-        if len(it.multi_index) == 0:
-            left_x = np.array(x)
-            left_x -= h
-            right_x = np.array(x)
-            right_x += h
-        else:
-            if len(it.multi_index) == 1:
-                ix = ix[0]
-            left_x = np.array(x)
-            left_x[ix] -= h
-            right_x = np.array(x)
-            right_x[ix] += h
-
-
-        diff = f(right_x)[0] - f(left_x)[0]
-        numgrad = diff / (2.0 * h)
-
-        if hasattr(numgrad, "shape") and len(numgrad.shape) > 0:
-            numgrad = numgrad[ix]
-
-        # Compare gradients
-        reldiff = abs(numgrad - grad[ix]) / max(1, abs(numgrad), abs(grad[ix]))
-        if reldiff > 1e-4:
-            print "Gradient check failed."
-            print "First gradient error found at index %s" % str(ix)
-            print "Your gradient: %f \t Numerical gradient: %f" % (grad[ix], numgrad)
-            return
-        it.iternext() # Step to next dimension
-
-    print "Gradient check passed!"
 
 def sanity_check():
     """
@@ -107,5 +54,9 @@ def your_sanity_checks():
     gradcheck_naive(lambda x: score_to_loss_grad(x, l2), np.random.randn(4, 5))    # 2-D test
 
 if __name__ == "__main__":
-    sanity_check()
-    your_sanity_checks()
+    l1 = softmax(np.random.randn(3,))
+    l2 = softmax(np.random.randn(4, 5))
+    gradcheck_naive(lambda x: score_to_loss_grad(x, l1), np.random.randn(3,))    # 1-D test
+    gradcheck_naive(lambda x: score_to_loss_grad(x, l2), np.random.randn(4, 5))    # 2-D test
+    # sanity_check()
+    # your_sanity_checks()
