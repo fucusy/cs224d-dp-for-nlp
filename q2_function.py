1d309144c5c68c376058f3847e4084abb166ec2f
fucusy <fucus@qq.com>
2016-07-16 15:43:44 +0800

    fix bug when more dimension data

--- q2_function.py
+++ q2_function.py
@@ -7,8 +7,10 @@ def cross_category_loss_grad(pred, labels):
         n = 1
     else:
         n = pred.shape[0]
+
     cost = -1.0 * np.sum(np.log(pred) * labels) / n  #scalar
     grad = -1.0 * labels / pred / n # (n, Dy)
+
     return cost, grad
 
 
@@ -24,7 +26,12 @@ def score_to_loss_grad(score, labels):
     grad = (softmax_result - labels) / n
 
 
-    # my gradient
-    grad = cross_grad * softmax_grad
+    if len(softmax_grad.shape) < 3:
+        # my gradient
+        grad = softmax_grad.dot(cross_grad)
+    else:
+        grad = np.zeros(labels.shape)
+        for i in range(softmax_grad.shape[0]):
+            grad[i, :] = softmax_grad[i].dot(cross_grad[i])
 
     return cost, grad
